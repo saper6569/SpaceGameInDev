460ed86612da39f255f74534349cb50ef06b1830
playper3 <saper6569@gmail.com>
2021-08-24 19:28:52 -0700

    fixed MainMenu screen and added new assests.

--- core/src/com/playper3/space/Main.java
+++ core/src/com/playper3/space/Main.java
@@ -1,13 +1,11 @@
 package com.playper3.space;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
-import com.playper3.space.screens.GameScreen1;
 import com.playper3.space.screens.MainMenu;
 
 public class Main extends Game {
     public void create() {
-        this.setScreen(new GameScreen1());
+        setScreen(new MainMenu());
     }
 }
 
--- core/src/com/playper3/space/screens/MainMenu.java
+++ core/src/com/playper3/space/screens/MainMenu.java
@@ -1,28 +1,74 @@
 package com.playper3.space.screens;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.playper3.space.Main;
 
 public class MainMenu implements Screen {
 
+    Main main;
+
     private Stage stage;
     private Music menuSong;
+    private Texture playTexture;
+    private TextureRegion playTextureRegion;
+    private TextureRegionDrawable playDrawable;
+    private ImageButton play;
+
+    private OrthographicCamera camera;
+    private ExtendViewport viewport;
 
     @Override
     public void show() {
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-    }
 
+        playTexture = new Texture("testButton.png");
+        menuSong = Gdx.audio.newMusic(Gdx.files.internal("C418 - 0x10c.mp3"));
+
+        int origin_x = (Gdx.graphics.getWidth() - playTexture.getWidth()) / 2;
+        int origin_y = (Gdx.graphics.getHeight() - playTexture.getHeight()) / 2;
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        camera = new OrthographicCamera();
+        viewport = new ExtendViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera);
+
+        playTextureRegion = new TextureRegion(playTexture);
+        playDrawable = new TextureRegionDrawable(playTextureRegion);
+        play = new ImageButton(playDrawable);
+        play.setPosition(origin_x, origin_y);
+
+        play.addListener(new ClickListener(){
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                ((Game)Gdx.app.getApplicationListener()).setScreen(new GameScreen1());
+            }
+        });
+
+        stage.addActor(play);
+    }
     @Override
     public void render(float delta) {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act();
+        stage.draw();
     }
 
     @Override
@@ -47,6 +93,8 @@ public class MainMenu implements Screen {
 
     @Override
     public void dispose() {
+        stage.dispose();
+        playTexture.dispose();
 
     }
 }
