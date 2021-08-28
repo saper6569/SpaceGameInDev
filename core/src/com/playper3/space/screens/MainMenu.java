6cfad99a6720ce81116c95dd03a0608ccd7b80aa
playper3 <saper6569@gmail.com>
2021-08-27 18:45:36 -0700

    made minor changes to MainMenu

--- core/src/com/playper3/space/screens/MainMenu.java
+++ core/src/com/playper3/space/screens/MainMenu.java
@@ -77,7 +77,7 @@ public class MainMenu implements Screen {
     @Override
     public void render(float delta) {
         //screen render functions
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         stage.act();
