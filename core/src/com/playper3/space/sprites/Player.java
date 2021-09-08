3c9249e9dce55376a2a6d39344f1e71df3b85bc6
playper3 <saper6569@gmail.com>
2021-09-07 17:20:19 -0700

    removed run mechanic

--- core/src/com/playper3/space/sprites/Player.java
+++ core/src/com/playper3/space/sprites/Player.java
@@ -22,13 +22,7 @@ public class Player extends Sprite {
     private final Animation<TextureRegion> backward;
     private final Animation<TextureRegion> left;
     private final Animation<TextureRegion> right;
-/*
-    private final Animation<TextureRegion> forwardSprint;
-    private final Animation<TextureRegion> backwardSprint;
-    private final Animation<TextureRegion> leftSprint;
-    private final Animation<TextureRegion> rightSprint;
 
- */
     public World world;
     public Body b2dBody;
 
@@ -51,13 +45,6 @@ public class Player extends Sprite {
         backward = new Animation<TextureRegion>(FRAME_DURATION, backwardFrames, Animation.PlayMode.LOOP);
         left = new Animation<TextureRegion>(FRAME_DURATION, rightFrames, Animation.PlayMode.LOOP);
         right = new Animation<TextureRegion>(FRAME_DURATION, leftFrames, Animation.PlayMode.LOOP);
-/*
-        forwardSprint = new Animation<TextureRegion>(SPRINT_FRAME_DURATION, forwardFrames, Animation.PlayMode.LOOP);
-        backwardSprint = new Animation<TextureRegion>(SPRINT_FRAME_DURATION, backwardFrames, Animation.PlayMode.LOOP);
-        leftSprint = new Animation<TextureRegion>(SPRINT_FRAME_DURATION, rightFrames, Animation.PlayMode.LOOP);
-        rightSprint = new Animation<TextureRegion>(SPRINT_FRAME_DURATION, leftFrames, Animation.PlayMode.LOOP);
-
- */
     }
 
     public void definePlayer() {
@@ -74,7 +61,7 @@ public class Player extends Sprite {
         b2dBody.createFixture(fixtureDef);
     }
 
-    public void updateSprite(float dt) {
+    public void updateSprite() {
         setPosition(b2dBody.getPosition().x - getWidth() / 2, b2dBody.getPosition().y - getHeight() / 2);
         TextureRegion frame = getFrame();
         if (frame != null) {
@@ -83,28 +70,6 @@ public class Player extends Sprite {
     }
 
     public void playerMovement() {
-        /*
-        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-            if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-                b2dBody.setLinearVelocity(0, -PLAYER_SPEED_SPRINT);
-                elapsed_time += Gdx.graphics.getDeltaTime();
-            }
-            else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-                b2dBody.setLinearVelocity(0, PLAYER_SPEED_SPRINT);
-                elapsed_time += Gdx.graphics.getDeltaTime();
-            }
-            else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-                b2dBody.setLinearVelocity(-PLAYER_SPEED_SPRINT, 0);
-                elapsed_time += Gdx.graphics.getDeltaTime();
-            }
-            else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-                b2dBody.setLinearVelocity(PLAYER_SPEED_SPRINT, 0);
-            }
-            else {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-            }
-        }
-         */
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             b2dBody.setLinearVelocity(0,-PLAYER_SPEED);
             elapsed_time += Gdx.graphics.getDeltaTime();
@@ -129,30 +94,6 @@ public class Player extends Sprite {
 
     public TextureRegion getFrame () {
         TextureRegion region = null;
-/*
-        if (Gdx.input.isKeyPressed(Input.Keys.SHIFT_LEFT)) {
-            if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-                region = forwardSprint.getKeyFrame(elapsed_time, true);
-            }
-            else if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-                region = backwardSprint.getKeyFrame(elapsed_time, true);
-            }
-            else if (Gdx.input.isKeyPressed((Input.Keys.A))) {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-                region = rightSprint.getKeyFrame(elapsed_time, true);
-            }
-            else if (Gdx.input.isKeyPressed((Input.Keys.D))) {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-                region = leftSprint.getKeyFrame(elapsed_time, true);
-            }
-            else {
-                elapsed_time += Gdx.graphics.getDeltaTime();
-                region = def;
-            }
-        }
-        */
 
         if (Gdx.input.isKeyPressed(Input.Keys.S)) {
             elapsed_time += Gdx.graphics.getDeltaTime();
