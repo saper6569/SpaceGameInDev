ad2327ac8baec5d7c17de32969e138918688abd7
playper3 <saper6569@gmail.com>
2021-09-13 18:49:19 -0700

    moved assets into respective files

---
+++ android/assets/GameScreen1/space.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="space" tilewidth="16" tileheight="16" tilecount="416" columns="13">
+ <grid orientation="orthogonal" width="16" height="96"/>
+ <image source="CosmicLilac_Tiles.png" width="208" height="128"/>
+</tileset>
